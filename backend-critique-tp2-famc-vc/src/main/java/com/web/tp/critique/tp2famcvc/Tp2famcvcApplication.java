29ea04acadd51da22ada2f06a396f30e940caa32
veroniquecloutier <2265278@cegeplimoilou.ca>
2024-11-22 13:19:58 -0500

    Ajouter le backend généré de base critique #2

---
+++ backend-critique-tp2-famc-vc/src/main/java/com/web/tp/critique/tp2famcvc/Tp2famcvcApplication.java
@@ -0,0 +1,13 @@
+package com.web.tp.critique.tp2famcvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp2famcvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp2famcvcApplication.class, args);
+	}
+
+}
