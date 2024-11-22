5577a74749b96ea29e2f9792e209a67ba54a3186
Felix-Alexandre Morneau-Carrier <129521661+Padaf3t@users.noreply.github.com>
2024-11-22 15:18:56 -0500

    Créer les exceptions pour le produit #2 #8
    information invalide et produit non trouvé

---
+++ backend-produit-tp2-famc-vc/src/main/java/com/web/tp/produit/tp2famcvc/exception/ProduitInformationInvalidException.java
@@ -0,0 +1,5 @@
+package com.web.tp.produit.tp2famcvc.exception;
+
+public class ProduitInformationInvalidException extends RuntimeException{
+    public ProduitInformationInvalidException(String message){super(message);}
+}
---
+++ backend-produit-tp2-famc-vc/src/main/java/com/web/tp/produit/tp2famcvc/exception/ProduitNonTrouveException.java
@@ -0,0 +1,7 @@
+package com.web.tp.produit.tp2famcvc.exception;
+
+public class ProduitNonTrouveException extends RuntimeException {
+    public ProduitNonTrouveException(String message) {
+        super(message);
+    }
+}
