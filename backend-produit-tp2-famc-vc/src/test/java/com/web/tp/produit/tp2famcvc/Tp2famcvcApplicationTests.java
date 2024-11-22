fe8f25f11f52b3f10a60e2a365b7968f9cce7813
Felix-Alexandre Morneau-Carrier <129521661+Padaf3t@users.noreply.github.com>
2024-11-22 13:16:49 -0500

    Ajouter backend généré de base produit #2

---
+++ backend-produit-tp2-famc-vc/src/test/java/com/web/tp/produit/tp2famcvc/Tp2famcvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.tp.produit.tp2famcvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp2famcvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
