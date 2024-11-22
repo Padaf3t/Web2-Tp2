37449e8192feb9c60e13cb922c8dd54d5389ebec
Felix-Alexandre Morneau-Carrier <129521661+Padaf3t@users.noreply.github.com>
2024-11-22 14:22:50 -0500

    Créer le repository de produit #2 #8

---
+++ backend-produit-tp2-famc-vc/src/main/java/com/web/tp/produit/tp2famcvc/repository/ProduitRepository.java
@@ -0,0 +1,8 @@
+package com.web.tp.produit.tp2famcvc.repository;
+
+import com.web.tp.produit.tp2famcvc.model.Produit;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProduitRepository extends CrudRepository<Produit, Long> {
+    Produit findFirstByEIDR(int EIDR);
+}
