191e82b5289fdda70211a04376c0e9571b286e81
veroniquecloutier <2265278@cegeplimoilou.ca>
2024-11-22 15:16:33 -0500

    Créer l'exception CritiqueInformationInvalidException #9 #2

---
+++ backend-critique-tp2-famc-vc/src/main/java/com/web/tp/critique/tp2famcvc/exception/CritiqueInformationInvalidException.java
@@ -0,0 +1,7 @@
+package com.web.tp.critique.tp2famcvc.exception;
+
+public class CritiqueInformationInvalidException extends RuntimeException {
+    public CritiqueInformationInvalidException(String message) {
+        super(message);
+    }
+}
