d008047e8cbc9dd4848534ce71564e9c6e5d7bf5
Felix-Alexandre Morneau-Carrier <129521661+Padaf3t@users.noreply.github.com>
2024-11-22 15:17:57 -0500

    Créer le validateur de produit #2 #8

---
+++ backend-produit-tp2-famc-vc/src/main/java/com/web/tp/produit/tp2famcvc/verification/ProduitValidateur.java
@@ -0,0 +1,57 @@
+package com.web.tp.produit.tp2famcvc.verification;
+
+import com.web.tp.produit.tp2famcvc.model.Produit;
+
+public class ProduitValidateur {
+
+    public String validateProduit(Produit produit) {
+        String messageErreur = "";
+
+        if(produit == null){
+            messageErreur = "Produit null";
+        }
+        else{
+            if(!validerStringsRemplies(produit.getNom(),produit.getRealisateur(),produit.getPaysOrigine(),produit.getAfficheSrc())){
+                messageErreur += "Champs non remplis";
+            }
+            if(!validerLongueurMaxString(256, produit.getNom(),produit.getRealisateur(),produit.getPaysOrigine(),produit.getAfficheSrc())){
+                messageErreur += "Champs trop long";
+            }
+            if(!validerNombrePositif(produit.getEIDR(), produit.getDureeMinute())){
+                messageErreur += "Champs doivent être positif";
+            }
+        }
+
+        return messageErreur;
+
+    }
+
+    private boolean validerNombrePositif(double... listeItems) {
+        for (double item : listeItems) {
+            if (item < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean validerStringsRemplies(String... listeItems) {
+        for (String item : listeItems) {
+            if (item == null || item.trim().isEmpty()) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean validerLongueurMaxString(int longueurMax, String... listeItems) {
+        for (String item : listeItems) {
+            if (item != null && item.length() > longueurMax) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+}
