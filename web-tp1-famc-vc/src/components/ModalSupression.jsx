fd1d994d47f5dab1e1d9a3a91f832c1adfffb983
Véronique Cloutier <145469349+veroniquecloutier@users.noreply.github.com>
2024-12-03 13:56:50 -0500

    Styliser le modal de confirmation de suppression d'item #7

--- web-tp1-famc-vc/src/components/ModalSupression.jsx
+++ web-tp1-famc-vc/src/components/ModalSupression.jsx
@@ -1,19 +1,20 @@
 import { Modal, Button } from "react-bootstrap";
+import style from '../style/Modal.module.css';
 
 const ModalSupression = ({ show, handleClose, onConfirm, bodyTexte }) => {
     return (
-        <Modal show={show} onHide={handleClose}>
-            <Modal.Header closeButton>
-                <Modal.Title>Confirmation de suppression</Modal.Title>
+        <Modal show={show} onHide={handleClose} dialogClassName={style.modalContent}>
+            <Modal.Header className={style.modalBg}>
+                <Modal.Title className={style.modalTitle}>Confirmation de suppression</Modal.Title>
             </Modal.Header>
-            <Modal.Body>
+            <Modal.Body className={style.modalBg}>
                 {bodyTexte}
             </Modal.Body>
-            <Modal.Footer>
-                <Button variant="secondary" onClick={handleClose}>
+            <Modal.Footer className={style.modalBg}>
+                <Button onClick={handleClose} className={style.annulerButton}>
                     Annuler
                 </Button>
-                <Button variant="danger" onClick={onConfirm}>
+                <Button onClick={onConfirm} className={style.supprimerButton}>
                     Supprimer
                 </Button>
             </Modal.Footer>
