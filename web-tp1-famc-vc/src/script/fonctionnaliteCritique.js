eb3aaedca4e17a384adb4fb83e70679ae64cccf8
Véronique Cloutier <145469349+veroniquecloutier@users.noreply.github.com>
2024-12-04 09:10:16 -0500

    Mettre à jour la doc de fonctionnaliteCritique.js #10

--- web-tp1-famc-vc/src/script/fonctionnaliteCritique.js
+++ web-tp1-famc-vc/src/script/fonctionnaliteCritique.js
@@ -8,6 +8,10 @@ import {useState} from "react";
  * @param {Event} event - L'événement de soumission du formulaire.
  * @param {function} setListeCritiques - Fonction pour mettre à jour la liste des critiques.
  * @param {function} setMessageErreur - Fonction pour afficher un message d'erreur.
+ * @param {function} setErreurPresente - Fonction pour signaler s'il y a des erreurs dans un ou des chmaps du formulaire.
+ * @param triggerCritiqueRefetch - State pour engendrer le refetch des critiques.
+ * @param {function} handleBoutonAfficherForm - Fonction pour afficher le formulaire.
+ * @param {function} setError - Fonction pour set le state error.
  */
 export async function ajouterNouvelleCritique(event, setListeCritiques, setMessageErreur, setErreurPresente, triggerCritiqueRefetch, handleBoutonAfficherForm, setError) {
     event.preventDefault();
@@ -25,8 +29,11 @@ export async function ajouterNouvelleCritique(event, setListeCritiques, setMessa
     try {
         let nouvelleCritique = await addCritique(nouvelleCritiqueInfo);
         setListeCritiques((anciennesCritiques) => [nouvelleCritique, ...anciennesCritiques]);
+        // Réactualiser les critiques
         triggerCritiqueRefetch();
+        // Cacher le formulaire
         handleBoutonAfficherForm();
+        // Reset le message d'erreur
         setMessageErreur("");
     }
     catch (e) {
@@ -37,15 +44,18 @@ export async function ajouterNouvelleCritique(event, setListeCritiques, setMessa
 }
 
 /**
- * Valide une nouvelle critique.
+ * Valide une nouvelle critique (champ par champ), et met en évidence les champs problématiques s'il y a lieu,
+ * en plus de déterminer un message d'erreur à afficher s'il y a lieu
  *
  * @param {FormData} formData - Les données du formulaire.
  * @param {object} nouvelleCritique - La nouvelle critique.
  * @param {function} setMessageErreur - Fonction pour afficher un message d'erreur.
+ * @param {function} setErreurPresente - Fonction pour signaler s'il y a des erreurs dans un ou des chmaps du formulaire.
  * @returns {boolean} - True si la critique est valide, false sinon.
  */
-function validerNouvelleCritique(formData, nouvelleCritique, setMessageErreur,setErreurPresente) {
+function validerNouvelleCritique(formData, nouvelleCritique, setMessageErreur, setErreurPresente) {
 
+    // Réinitialiser
     let erreurPresente = false;
     let valide = true;
     setMessageErreur(() => "");
@@ -113,12 +123,13 @@ function obtenirNouvelleCritique(formData) {
 }
 
 /**
- * Retire une critique de la liste.
+ * Retire une critique de la liste selon son ID.
  *
  * @param {Event} event - L'événement de clic sur le bouton de suppression.
  * @param {number} id - L'identifiant de la critique à supprimer.
  * @param {function} setListeCritiques - Fonction pour mettre à jour la liste des critiques.
- * @param triggerCritiqueRefetch
+ * @param triggerCritiqueRefetch - State pour engendrer le refetch des critiques.
+ * @param {function} setError - Fonction pour set le state error.
  */
 export async function retirerCritiqueParId(event, id, setListeCritiques, triggerCritiqueRefetch, setError) {
     event.preventDefault();
@@ -132,10 +143,17 @@ export async function retirerCritiqueParId(event, id, setListeCritiques, trigger
         console.log('La destruction de ' + id + ' n\'a pas fonctionné');
         setError({error: "error", message: e.message});
     }
-
-
 }
 
+/**
+ * Retire toutes les critiques de la liste qui ont un certain EIDR.
+ *
+ * @param {Event} event - L'événement de clic sur le bouton de suppression.
+ * @param {number} eidr - L'EIDR du produit dont il faut supprimer les critiques.
+ * @param {function} setListeCritiques - Fonction pour mettre à jour la liste des critiques.
+ * @param triggerCritiqueRefetch - State pour engendrer le refetch des critiques.
+ * @param {function} setError - Fonction pour set le state error.
+ */
 export async function retirerCritiquesParEidr(event, eidr, setListeCritiques, triggerCritiqueRefetch, setError) {
     event.preventDefault();
 
@@ -153,7 +171,7 @@ export async function retirerCritiquesParEidr(event, eidr, setListeCritiques, tr
 /**
  * Obtient le nom du produit associé à un EIDR.
  *
- * @param {number} EIDR - L'identifiant unique du produit.
+ * @param {number} eidr - L'identifiant unique du produit.
  * @param {array} listeProduits - La liste de tous les produits.
  * @returns {string} - Le nom du produit, ou "Produit inconnu" si le produit n'est pas trouvé.
  */
