0d43c916697fc44687b02ff04b0fab133c0b53fb
Felix-Alexandre Morneau-Carrier <129521661+Padaf3t@users.noreply.github.com>
2024-12-04 09:00:48 -0500

    Documenter la SectionTitre.jsx #10

--- web-tp1-famc-vc/src/components/SectionTitres.jsx
+++ web-tp1-famc-vc/src/components/SectionTitres.jsx
@@ -1,10 +1,20 @@
+/**
+ * Composant gérant la section des titres de sections et du boutons pour afficher le formulaire
+ * @param {function} handleBoutonProduit - Permet d'handle le comportement du bouton produit
+ * @param {function} handleBoutonCritique - Permet d'handle le comportement du bouton critique
+ * @param {function} handleBoutonStatistique - Permet d'handle le comportement du bouton statistique
+ * @param {function} handleBoutonAfficherForm - Permet d'handle le comportement du bouton du formulaire
+ * @param {boolean} isShowingForm - Formulaire afficher ou non
+ * @param {boolean} isShowingStatistique - Section statistique afficher ou non
+ * @param {boolean} isShowingProduit  - Section produit afficher ou non
+ * @param {boolean} isShowingCritique - Section critique afficher ou non
+ * @param {boolean} produitEnModification - produit est en modification ou non
+ * @returns {JSX.Element}
+ * @constructor
+ */
 export default function SectionTitres({handleBoutonProduit,handleBoutonCritique,handleBoutonStatistique,handleBoutonAfficherForm,isShowingForm,isShowingStatistique,isShowingProduit,isShowingCritique,produitEnModification}){
 
-    let textBoutonAjouter = "Ajouter";
-    if(isShowingForm){
-        textBoutonAjouter = "Annuler";
-    }
-
+    let textBoutonAjouter = isShowingForm ? "Annuler" : "Ajouter";
 
     return(
         <>
