fe69e664684012245b86bb74082f6d17a76f2ef1
Felix-Alexandre Morneau-Carrier <129521661+Padaf3t@users.noreply.github.com>
2024-12-04 09:05:23 -0500

    Documenter le ModalSuppression.jsx

--- web-tp1-famc-vc/src/components/ModalSupression.jsx
+++ web-tp1-famc-vc/src/components/ModalSupression.jsx
@@ -1,6 +1,15 @@
 import { Modal, Button } from "react-bootstrap";
 import style from '../style/Modal.module.css';
 
+/**
+ * La fenêtre popup de confirmation de suppression
+ * @param show - Si afficher ou non
+ * @param handleClose - Fonction permettant de fermer le modal
+ * @param onConfirm - Fonction lors de la confirmation du modal
+ * @param bodyTexte - Texte afficher dans le body du modal
+ * @returns {JSX.Element}
+ * @constructor
+ */
 const ModalSupression = ({ show, handleClose, onConfirm, bodyTexte }) => {
     return (
         <Modal show={show} onHide={handleClose} dialogClassName={style.modalContent}>
