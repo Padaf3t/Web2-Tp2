4675cea730b7a88ce8f36c3a65055d42b1a655e3
Véronique Cloutier <145469349+veroniquecloutier@users.noreply.github.com>
2024-12-04 09:22:36 -0500

    Documenter httpCritiques.js #10

--- web-tp1-famc-vc/src/script/httpCritiques.js
+++ web-tp1-famc-vc/src/script/httpCritiques.js
@@ -1,3 +1,9 @@
+/**
+ * Récupère de manière asynchrone toutes les critiques disponibles depuis l'API backend.
+ *
+ * @returns {Promise<Array>} Une promesse qui résout en un tableau de critiques.
+ * @throws {Error} Si une erreur se produit lors de la récupération des critiques.
+ */
 export async function fetchAvailableCritiquesAsync() {
     const response = await fetch('http://localhost:8080/critiques');
     if (!response.ok) {
@@ -7,6 +13,13 @@ export async function fetchAvailableCritiquesAsync() {
     return resData;
 }
 
+/**
+ * Ajoute une nouvelle critique à l'API backend.
+ *
+ * @param {Object} critique L'objet critique à ajouter.
+ * @returns {Promise<Object>} Une promesse qui résout en la critique ajoutée.
+ * @throws {Error} Si une erreur se produit lors de l'ajout de la critique.
+ */
 export async function addCritique(critique) {
     const response = await fetch('http://localhost:8080/critique/post',
         {
@@ -27,6 +40,12 @@ export async function addCritique(critique) {
     return resData;
 }
 
+/**
+ * Supprime une critique de l'API backend en utilisant son ID.
+ *
+ * @param {number} id L'ID de la critique à supprimer.
+ * @throws {Error} Si une erreur se produit lors de la suppression de la critique.
+ */
 export async function deleteCritiqueById(id) {
     const response = await fetch('http://localhost:8080/critique/deletebyid/' + id,
         {
@@ -39,6 +58,12 @@ export async function deleteCritiqueById(id) {
     }
 }
 
+/**
+ * Supprime des critiques de l'API backend en utilisant leur EIDR.
+ *
+ * @param {number} eidr L'EIDR des critiques à supprimer.
+ * @throws {Error} Si une erreur se produit lors de la suppression des critiques.
+ */
 export async function deleteCritiqueByEidr(eidr) {
     const response = await fetch('http://localhost:8080/critique/deletebyeidr/' + eidr,
         {
